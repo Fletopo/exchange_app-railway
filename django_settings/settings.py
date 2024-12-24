dce3890d5272b8c8666ebd2bea7b0b25115fdfa0
Fletopo <fl3t0p0@gmail.com>
2024-12-24 19:47:23 -0300

    listo para testin

--- django_settings/settings.py
+++ django_settings/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'channels',
     'rest_framework.authtoken',
+    'whitenoise.runserver_nostatic',
     'users',
     'publication',
     'chat',
@@ -73,6 +74,7 @@ REST_FRAMEWORK = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "white_noise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -156,6 +158,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
