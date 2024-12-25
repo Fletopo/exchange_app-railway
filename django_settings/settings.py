cc9f7d483ad1d285be67e2b554ff39dfa075f931
Fletopo <fl3t0p0@gmail.com>
2024-12-25 02:02:39 -0300

    cambio de procfile

--- django_settings/settings.py
+++ django_settings/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!=muqgsv28b6zr=iqinkpz5f$k9s%ekz#3b8i0ze8n)t09z_t6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
